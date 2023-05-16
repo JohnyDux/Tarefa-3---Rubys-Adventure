bced4d25e5f9811bcc419c3d8f2a735a8f966d56
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-05-16 01:25:03 +0100

    Audio Finished

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -10,6 +10,9 @@ public class RubyController : MonoBehaviour
 
     public GameObject projectilePrefab;
 
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -99,6 +102,8 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
+
+            PlaySound(hitSound);
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
@@ -114,6 +119,8 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+
+        PlaySound(throwSound);
     }
 
     public void PlaySound(AudioClip clip)
